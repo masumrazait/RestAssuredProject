fc5ba321befddf9c6dacd85864234014a4a32c94
masumrazait <masumrazait@gmail.com>
2026-01-26 18:17:54 +0530

    added new code

---
+++ src/test/java/RestAssuredTest/Demo1.java
@@ -0,0 +1,13 @@
+package RestAssuredTest;
+
+/*
+given()- set cookies,add auth, add param, set header, info etc....
+
+when()= get ,post, patch,put, delete
+
+then()= validate the status code and request body and header
+
+*/
+public class Demo1 {
+
+}
