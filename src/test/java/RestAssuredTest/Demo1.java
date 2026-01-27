c2c8e6be04e737e3e4453b67139e29f0e82ada9c
masumrazait <masumrazait@gmail.com>
2026-01-27 11:31:29 +0530

    code modification working now

--- src/test/java/RestAssuredTest/Demo1.java
+++ src/test/java/RestAssuredTest/Demo1.java
@@ -1,5 +1,8 @@
 package RestAssuredTest;
 
+import static io.restassured.RestAssured.given;
+import org.testng.annotations.Test;
+
 /*
 given()- set cookies,add auth, add param, set header, info etc....
 
@@ -9,5 +12,8 @@ then()- validate the status code and request body and header
 
 */
 public class Demo1 {
-
+	@Test
+	public void getProductList() {
+		given().when().get("https://automationexercise.com/api/productsList?id=1").then().statusCode(200);
+	}
 }
