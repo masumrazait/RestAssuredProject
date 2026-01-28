295ec8e2ad37f79fb91202f0ba054798064090c9
masumrazait <masumrazait@gmail.com>
2026-01-28 17:19:02 +0530

    added new class

--- src/test/java/RestAssuredTest/ComplexJsonParse.java
+++ src/test/java/RestAssuredTest/ComplexJsonParse.java
@@ -6,7 +6,14 @@ import io.restassured.path.json.JsonPath;
 public class ComplexJsonParse {
 	public static void main(String[] args) {
 		JsonPath js = new JsonPath(payload.CoursePrice());
-
+		int count = js.getInt("courses.size()");
+		System.out.println("Number of courses in list : " + count);
+		int totalAmount = js.getInt("dashboard.purchaseAmount");
+		System.out.println(totalAmount);
+		String CourseTitle = js.get("courses[2].title");
+		System.out.println(CourseTitle);
+		String CourseTitle2 = js.get("courses[3].title");
+		System.out.println(CourseTitle2);
 	}
 
 }
