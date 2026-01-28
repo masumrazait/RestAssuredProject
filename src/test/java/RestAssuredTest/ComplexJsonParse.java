5864c707c837a578b5a52f3b704b049e0949a6cb
masumrazait <masumrazait@gmail.com>
2026-01-28 20:56:50 +0530

    Added new code

--- src/test/java/RestAssuredTest/ComplexJsonParse.java
+++ src/test/java/RestAssuredTest/ComplexJsonParse.java
@@ -4,7 +4,7 @@ import files.payload;
 import io.restassured.path.json.JsonPath;
 
 public class ComplexJsonParse {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		JsonPath js = new JsonPath(payload.CoursePrice());
 		int count = js.getInt("courses.size()");
 		System.out.println("Number of courses in list : " + count);
@@ -14,6 +14,13 @@ public class ComplexJsonParse {
 		System.out.println(CourseTitle);
 		String CourseTitle2 = js.get("courses[3].title");
 		System.out.println(CourseTitle2);
-	}
 
+		// Print all Courses title and there price;
+		for (int i = 0; i < count; i++) {
+			String courseTitle = js.get("courses[" + i + "].title");
+			int coursePrice = js.getInt("courses[" + i + "].price"); // if you also want price
+			Thread.sleep(300);
+			System.out.println(courseTitle + " : " + coursePrice);
+		}
+	}
 }
