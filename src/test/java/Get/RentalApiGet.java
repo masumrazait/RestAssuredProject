f2cd96d4c472326f343defe7ba681356b6c72869
masumrazait <masumrazait@gmail.com>
2026-02-01 19:23:31 +0530

    Addded new code for OauthTest

---
+++ src/test/java/Get/RentalApiGet.java
@@ -0,0 +1,15 @@
+package Get;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+
+public class RentalApiGet {
+	public static void main(String[] args) {
+		RestAssured.baseURI = "https://simple-tool-rental-api.glitch.me";
+		String response = given().queryParam("id", "6483").queryParam("category", "trailers").when().get("/tools")
+				.then().assertThat().log().all().statusCode(200).extract().asString();
+
+		System.out.println(response);
+	}
+}
