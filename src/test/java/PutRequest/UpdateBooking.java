7becd24766425b5c5b3072c3b67250616706d093
masumrazait <masumrazait@gmail.com>
2026-02-12 00:21:32 +0530

    Added new code UpdateBooking

---
+++ src/test/java/PutRequest/UpdateBooking.java
@@ -0,0 +1,18 @@
+package PutRequest;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+
+public class UpdateBooking {
+	public static void main(String[] args) {
+		RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+		String reso = given().log().all().header("Content-Type", "application/json").header("Accept", "application/json")
+				.header("Cookie", "token=abc123")
+				.body("{\r\n" + "  \"firstname\": \"SOne\",\r\n" + "  \"lastname\": \"Brown\",\r\n"
+						+ "  \"totalprice\": 111,\r\n" + "  \"depositpaid\": true,\r\n" + "  \"bookingdates\": {\r\n"
+						+ "    \"checkin\": \"2018-01-01\",\r\n" + "    \"checkout\": \"2019-01-01\"\r\n" + "  },\r\n"
+						+ "  \"additionalneeds\": \"Breakfast\"\r\n" + "}")
+				.when().put("/booking/1").then().assertThat().log().all().statusCode(403).extract().toString();
+	}
+}
