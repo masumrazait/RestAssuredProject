a936d1657e452c203b46a5e3464a874468b73128
masumrazait <masumrazait@gmail.com>
2026-02-12 17:12:14 +0530

    Added new code UpdateBooking

--- src/main/java/Coforge/StringCompressed.java
+++ src/main/java/Coforge/StringCompressed.java
@@ -5,13 +5,15 @@ import java.util.Map;
 
 public class StringCompressed {
 	public static void main(String[] args) {
-		String str = "aaaaajjjjjjj";
+		String str = "aaab ccd eef";
 		String result = "";
 		Map<Character, Integer> charc = new LinkedHashMap<>();
 		for (char c : str.toCharArray()) {
+			if(c!=' '&& c!='b')
 			charc.put(c, charc.getOrDefault(c, 0) + 1);
 		}
 		for (Map.Entry<Character, Integer> entry : charc.entrySet()) {
+			if(entry.getValue()>1)
 			result = result + entry.getKey() + entry.getValue();
 		}
 		System.out.println(result);
