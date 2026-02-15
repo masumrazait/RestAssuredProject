8658343896e09831bda105f8e6aeea3a01de478b
masumrazait <masumrazait@gmail.com>
2026-02-15 23:13:55 +0530

    added new code

---
+++ src/main/java/mphasis/UpperCaseCountInString.java
@@ -0,0 +1,15 @@
+package mphasis;
+
+public class UpperCaseCountInString {
+	public static void main(String[] args) {
+		String str = "PatnaDelhiKolkataBiharAmnourSARAn";
+		int count=0;
+		for(char c:str.toCharArray()) {
+			if(Character.isUpperCase(c)) {
+				System.out.print(c+" ");
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
