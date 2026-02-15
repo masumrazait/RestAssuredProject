fd84dac7c9104791495c7456541c90ff50968dfc
masumrazait <masumrazait@gmail.com>
2026-02-15 22:18:38 +0530

    added new code

---
+++ src/main/java/mphasis/StringCompression.java
@@ -0,0 +1,19 @@
+package mphasis;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StringCompression {
+	public static void main(String[] args) {
+		String str = "dddddssss";
+		String result = "";
+		Map<Character, Integer> charc = new LinkedHashMap<>();
+		for (char c : str.toCharArray()) {
+			charc.put(c, charc.getOrDefault(c, 0) + 1);
+		}
+		for (Map.Entry<Character, Integer> entry : charc.entrySet()) {
+			result = result + entry.getKey() + entry.getValue();
+		}
+		System.out.println(result);
+	}
+}
