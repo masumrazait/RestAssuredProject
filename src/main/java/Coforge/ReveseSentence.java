b5e09260b5dd76c19126a4022fc131aaf76cdf41
masumrazait <masumrazait@gmail.com>
2026-02-19 19:10:52 +0530

    added new code DynamicWebTable

---
+++ src/main/java/Coforge/ReveseSentence.java
@@ -0,0 +1,18 @@
+package Coforge;
+
+public class ReveseSentence {
+	public static void main(String[] args) {
+		String str = "ihled antap rahib";
+		String words[] = str.split(" ");
+		String revSen = "";
+		for (String word : words) {
+			String revWord = "";
+			int len = word.length();
+			for (int i = len - 1; i >= 0; i--) {
+				revWord = revWord + word.charAt(i);
+			}
+			revSen = revSen + revWord + " ";
+		}
+		System.out.println(revSen);
+	}
+}
