e178f00954841aa8d645351dc16d4933f2eba467
masumrazait <masumrazait@gmail.com>
2026-02-20 19:57:43 +0530

    added new code

--- src/main/java/mphasis/RevSentece.java
+++ src/main/java/mphasis/RevSentece.java
@@ -1,5 +1,12 @@
 package mphasis;
 
 public class RevSentece {
-
+	public static void main(String[] args) {
+		String str = "masum123raza";
+		String rev = "";
+		for (int i = str.length()-1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
+		}
+		System.out.println(rev);
+	}
 }
