7e74b4dacca6216b5324730b07600834f17c50b7
masumrazait <masumrazait@gmail.com>
2026-02-20 20:05:28 +0530

    code modification

--- src/main/java/mphasis/RevSentece.java
+++ src/main/java/mphasis/RevSentece.java
@@ -8,5 +8,18 @@ public class RevSentece {
 			rev = rev + str.charAt(i);
 		}
 		System.out.println(rev);
+		
+		
+		String input = "masum123raza senetee raza delhi patna";
+		String Sentrev = "";
+		String words[]=input.split(" ");
+		for(String word:words) {
+			String Wordrev="";
+			for (int i = word.length()-1; i >= 0; i--) {
+			Wordrev = Wordrev + word.charAt(i);
+		}
+			Sentrev=Sentrev+Wordrev+" ";
+	}
+		System.out.println(Sentrev);
 	}
 }
