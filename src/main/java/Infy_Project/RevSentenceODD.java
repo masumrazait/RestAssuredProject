c93659499bee3e28ebab1bd5d50d0ee18f49df0c
masumrazait <masumrazait@gmail.com>
2026-02-22 19:30:20 +0530

    added new code

--- src/main/java/Infy_Project/RevSentenceODD.java
+++ src/main/java/Infy_Project/RevSentenceODD.java
@@ -2,11 +2,11 @@ package Infy_Project;
 
 public class RevSentenceODD {
 	public static void main(String[] args) {
-		String str = "delhi ihled patna antap amnour ruonma";
+		String str = "ihled ihled antap antap ruonma ruonma";
 		String RevSen = "";
 		String words[] = str.split(" ");
 		for (int i = 0; i < words.length; i++) {
-			if (i % 2 != 0) {
+			if (i % 2 == 0) {
 				String RevWord = "";
 				for (int j = words[i].length() - 1; j >= 0; j--) {
 					RevWord = RevWord + words[i].charAt(j);
