1ee72918e13eaac5f119b2f9fe0cb2c1010f64ba
masumrazait <masumrazait@gmail.com>
2026-02-22 19:40:09 +0530

    added new code

--- src/main/java/Infy_Project/StringCompress.java
+++ src/main/java/Infy_Project/StringCompress.java
@@ -2,15 +2,6 @@ package Infy_Project;
 
 public class StringCompress {
 	public static void main(String[] args) {
-		String str = "a3v4";
-		String result = "";
-		for (int i = 0; i < str.length() - 1; i += 2) {
-			char c = str.charAt(i);
-			int count = Character.getNumericValue(str.charAt(i + 1));
-			for (int j = 1; j <= count; j++) {
-				result = result + c;
-			}
-		}
-		System.out.println(result);
+		
 	}
 }
